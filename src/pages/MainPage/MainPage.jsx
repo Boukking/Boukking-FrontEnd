226140d89b16248f8187310a7f0ead65ee86d1b1
Huguiz <hugo.maurin@hotmail.com>
2024-05-03 17:16:11 +0200

    Header of the first page done

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,30 +1,76 @@
 //module
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+//component
+import Register from '../../components/Register/Register';
 
 // style
 import s from './MainPage.module.css';
 
 export default function MainPage() {
+    const [displayRegisterPage, setDisplayRegisterPage] = useState(false);
+    const [displayLoginPage, setDisplayLoginPage] = useState(false);
+
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const [searchBar, setSearchBar] = useState("");
+    const [fromDate, setFromDate] = useState("");
+    const [toDate, setToDate] = useState("");
+    const [numberOfPersons, setNumberOfPersons] = useState("");
+
+    const [offers, setOffers] = useState([]);
+
+    function handleForm(e) {
+        e.preventDefault();
+        if (!searchBar) return setErrorMessage("Fill the place to visit");
+        if (!fromDate || !toDate) return setErrorMessage("Fill the date");
+        if (new Date(toDate) <= new Date(fromDate)) return setErrorMessage("Error, please view the date");
+        if (!numberOfPersons) setNumberOfPersons(1);
+    }
+
     return (
         <>
+        
             <header className={s.header}>
                 <div className={`page`}>
                     <h1>Boukking</h1>
                     <div>
                         <Link>Add your dwelling</Link>
-                        <button>Register</button>
-                        <button>Sign in</button>
+                        <button onClick={() => setDisplayRegisterPage(!displayRegisterPage)}>Register</button>
+                        <button onClick={() => setDisplayLoginPage(!displayLoginPage)}>Sign in</button>
+                        {displayRegisterPage && <Register setDisplayRegisterPage={setDisplayRegisterPage} />}
                     </div>
+                    
                 </div>
+                
                 <div className={`page`}>
-                    <h2>
-                        Find out your next journey
-                    </h2>
+                    <h2>Find out your next journey</h2>
+                    <p>Search for deals on hotels, self-catering accommodations and more...</p>
                 </div>
+                {errorMessage && <p className='page' id={s.error_message}>{errorMessage}</p>}
+                <form onSubmit={handleForm} className='page'>
+                    <input id={s.place_input} type="text" placeholder="The place to visit" value={searchBar} onChange={(e) => setSearchBar(e.target.value)} />
+                    <input type="date" value={fromDate} onChange={(e) => setFromDate(e.target.value)} />
+                    <p>→</p>
+                    <input type="date" value={toDate} onChange={(e) => setToDate(e.target.value)} />
+                    <input type="number" min="1" max="25" placeholder='1 person' value={numberOfPersons} onChange={(e) => setNumberOfPersons(e.target.value)} />
+                    <button>Search</button>
+                </form>
             </header>
             <footer className={s.footer}>
                 <div className={`page`}>
-                    <h1>1</h1>
+                    <h1>Take a look at the latest offers added by users :</h1>
+                    <article>
+                        {
+                            !offers.length ?
+                                <p>No offers to show for the moment</p>
+                                :
+                                offers.map(offer => {
+                                    console.log("offer")
+                                })
+                        }
+                    </article>
                 </div>
             </footer>
         </>
